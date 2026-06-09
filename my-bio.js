5e6e449743c60726423022cc3bb00aa52b2b5903
Kaito Udagawa <umireon@kaito.tokyo>
2026-06-09 15:24:47 +0900

    Create page
    Signed-off-by: Kaito Udagawa <umireon@kaito.tokyo>

--- my-bio.js
+++ my-bio.js
@@ -2,7 +2,5 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-customElements.define("my-bio",
-  class extends HTMLElement {
-  }
-);
+customElements.define("my-bio", class extends HTMLElement {});
+customElements.define("my-bio-link", class extends HTMLElement {});
